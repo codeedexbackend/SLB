6a92db6c225b45e9b6f9a1ff160d8e3da111df10
codeedexbackend <prithew.codeedex@gmail.com>
2024-05-15 16:56:13 +0530

    dp

--- backend/serializers.py
+++ backend/serializers.py
@@ -107,3 +107,8 @@ class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ['user', 'gate_pass_no', 'crew', 'designation', 'rig_or_rigless', 'project_name', 'company_name', 'profile_photo','full_name']
+
+class ProfilePhotoUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['profile_photo']
